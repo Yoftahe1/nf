41dd8d1e1d9b1746f7e97f9d131714f587229d6e
Yoftahe Merkebu <yoftahemerkebu2@gmail.com>
2024-12-25 19:34:07 +0300

    autofixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 define(function (require, exports, module) {
 
 	module.exports = {
+		autoprefixer: {},
 		darkMode: ['class'],
 		content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
 		prefix: '',
